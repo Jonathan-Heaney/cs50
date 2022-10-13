a9833a68ac6a39ad3f1824a0d31534a1373b154d
Jonathan Heaney <jonathan.heaney@gmail.com>
2022-10-13 16:08:00 -0500

    Refactored to add helper functions, comments

--- cs50.js
+++ cs50.js
@@ -2,16 +2,19 @@
 
 function validateCC(num) {
   let numString = num.toString();
+  // Validate the number is the right length
   if (
     numString.length !== 13 &&
     numString.length !== 15 &&
     numString.length !== 16
   )
     return 'INVALID';
+  // Validate the number passes the Luhn algorithm
   else if (!validateNum(numString)) return 'INVALID';
   else {
     const firstDigit = numString.charAt(0);
     const firstTwoDigits = numString.charAt(0) + numString.charAt(1);
+    // Check to see which brand of card it is
     if (firstTwoDigits === '34' || firstTwoDigits === '37') {
       return 'AMEX';
     } else if (
@@ -28,57 +31,70 @@ function validateCC(num) {
   }
 }
 
-console.log(validateCC(401288881881));
+console.log(validateCC(4003600000000014));
 
+// Check to see if the number is a valid CC number
 function validateNum(string) {
   const arr = string.split('');
-  //   console.log(arr);
   const operatorArr = [];
   const sumArr = [];
 
-  for (let i = string.length - 2; i >= 0; i -= 2) {
-    operatorArr.push(arr[i]);
+  // Add every other digit to a different array
+  for (let i = string.length; i >= 0; i -= 2) {
+    if (arr[i]) {
+      operatorArr.push(`${arr[i]}`);
+      sumArr.push(`${arr[i + 1]}`);
+    }
   }
 
-  for (let i = string.length - 1; i >= 0; i -= 2) {
-    sumArr.push(arr[i]);
-  }
-  //   console.log(operatorArr);
-  //   console.log(sumArr);
+  // Double each value in the operator array
+  const doubledArr = getDoubledArr(operatorArr);
 
-  const doubledArr = Array.from(operatorArr, (x) => x * 2);
-  //   console.log(doubledArr);
+  // Turn the doubled array into a string so it can be split
+  const stringArr = getStringArr(doubledArr);
 
-  const stringArr = doubledArr.map((num) => {
+  // Split the string array into an array of individual digits (formatted as strings)
+  const splitArr = getSplitArr(stringArr);
+
+  // Get the sum of the split array
+  const sum1 = getArraySum(splitArr);
+
+  // Get the sum of all the unused digits
+  const sum2 = getArraySum(sumArr);
+
+  // Get the sum of both sets of digits
+  const finalSum = sum1 + sum2;
+
+  //Check to see if the final sum ends in a 0; if so, it could be a valid CC number
+  if (finalSum % 10 !== 0) return 'INVALID';
+  else return true;
+}
+
+// Helper functions, used inside validateNum function
+function getDoubledArr(array) {
+  return Array.from(array, (x) => x * 2);
+}
+
+function getStringArr(array) {
+  const newArr = array.map((num) => {
     return String(num);
   });
+  return newArr;
+}
 
-  //   console.log(stringArr);
-
-  const splitArr = stringArr
+function getSplitArr(array) {
+  const newArr = array
     .map((element) => {
       return element.split('');
     })
     .flat();
 
-  //   console.log(splitArr);
-
-  const sum1 = splitArr.reduce((accumulator, value) => {
-    return Number(accumulator) + Number(value);
-  }, 0);
-
-  //   console.log(sum1);
-
-  const sum2 = sumArr.reduce((accumulator, value) => {
-    return Number(accumulator) + Number(value);
-  }, 0);
-
-  //   console.log(sum2);
-
-  const finalSum = sum1 + sum2;
-
-  if (finalSum % 10 !== 0) return 'INVALID';
-  else return true;
+  return newArr;
 }
 
-console.log(validateNum('4003600000000014'));
+function getArraySum(array) {
+  const sum = array.reduce((accumulator, value) => {
+    return Number(accumulator) + Number(value);
+  }, 0);
+  return sum;
+}
